6fcd41407fe07328dc61fcd513af6979a66cac82
mhoong <mhoong2@yahoo.com>
2022-11-04 04:07:43 -0600

    about me and modal

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
 console.log('Connected')
 
+const modal = document.getElementById('catModal');
 
+const button = document.getElementsByClassName('catButton');
+
+const catSpan = document.getElementsByClassName('close')[0];
+
+button.onclick = function() {
+    modal.style.display = 'block';
+}
+
+catSpan.onclick = function() {
+    modal.style.display = 'none';
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none"
+    }
+}
