74cfa5177b55a2a17843b0393fbf7e81576eec53
mhoong <mhoong2@yahoo.com>
2022-11-04 16:39:14 -0600

    Cat Modal made

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
-console.log('Connected')
+//W3schools
+// Get the modal
+const modal = document.getElementById("catModal");
 
-const modal = document.getElementById('catModal');
+// Get the button that opens the modal
+const btn = document.getElementById("catButton");
 
-const button = document.getElementsByClassName('catButton');
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName("close")[0];
 
-const catSpan = document.getElementsByClassName('close')[0];
-
-button.onclick = function() {
-    modal.style.display = 'block';
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
 }
 
-catSpan.onclick = function() {
-    modal.style.display = 'none';
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
 }
 
+// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none"
-    }
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
+
