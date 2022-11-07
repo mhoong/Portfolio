7074bf08ee85fd5a697eb4b61f629d7b91b3648e
mhoong <mhoong2@yahoo.com>
2022-11-07 03:12:45 -0700

    fixed resume js

--- js/resume.js
+++ js/resume.js
@@ -1,6 +1,6 @@
 console.log('connected')
 // Resume Download
-folkstalk.com
+// folkstalk.com
 const resumeLink = document.getElementById('resumepdf');
 resumeLink.download = 'Resume.pdf';
 resumeLink.dispatchEvent(new MouseEvent('click'));
